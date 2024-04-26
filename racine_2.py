b28841dd8367bea2f92a167e4b668b5c723245fc
PC MAISON <cosmowmarchal@gmail.com>
2024-04-26 20:42:40 +0200

    better errors management

--- racine_2.py
+++ racine_2.py
@@ -1,16 +1,26 @@
-# trying to approach 1,414 213 562 373 095 048 801 68 
+# trying to approach 1,414 213 562 373 095 048 801 68
+import decimal
+
+
 def decimal_expansion(depth):
-	depth -= 1
-	if depth == 0:
-		return 2
-	return 2 + (1/decimal_expansion(depth))
+    depth -= 1
+    if depth == 0:
+        return 2
+    return decimal.Decimal(2) + (decimal.Decimal(1) / decimal_expansion(depth))
+
+
 depth = input("Depth : ")
-try: 
-	depth = int(depth)
-	if depth < 1: 
-		print("La profondeur est supérieure à 1.")
-	else:
-		root_of_two = decimal_expansion(depth) - 1 # [1,2,2,2,...]
-		print(root_of_two)
-except: 
-	print("La profondeur est un nombre.")
+try:
+    depth = int(depth)
+    if depth < 1:
+        raise ValueError
+except ValueError as e:
+    print("La profondeur est un nombre plus grand que 1.")
+
+else:
+    try:
+        decimal.getcontext().prec = depth  #  sets maximum decimal values
+        root_of_two = decimal_expansion(depth) - 1  # [1,2,2,2,...]
+        print(root_of_two)
+    except Exception as error:
+        print(error)
